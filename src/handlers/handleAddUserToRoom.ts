8b30203e88e864b39b9061d9c9ecbf3e97c40ee8
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-11-16 20:40:50 +0400

    feat: create function for adding user to room

---
+++ src/handlers/handleAddUserToRoom.ts
@@ -0,0 +1,48 @@
+import { clients } from "../db/clients";
+import { games } from "../db/games";
+import { rooms } from "../db/rooms";
+import type { Game } from "../types/Game";
+import { generateId } from "../utils/generateId";
+import { getPlayerIndexByWs } from "../utils/getPlayerIndexByWs";
+import { sendMessage } from "../utils/sendMessage";
+import { updateRooms } from "../utils/updateRooms";
+
+export function handleAddUserToRoom(ws: WebSocket, data: any) {
+  const playerIndex = getPlayerIndexByWs(ws);
+  if (!playerIndex) return;
+
+  const room = rooms.get(data.indexRoom);
+  if (!room || room.players.length !== 1) return;
+
+  room.players.push(playerIndex);
+
+  const gameId = generateId();
+  const game: Game = {
+    gameId,
+    players: [...room.players],
+    playerShips: new Map(),
+    playerBoards: new Map(),
+    currentTurn: room.players[0],
+    started: false,
+  };
+
+  games.set(gameId, game);
+
+  room.players.forEach((pIndex, idx) => {
+    const playerWs = clients.get(pIndex);
+    if (playerWs) {
+      sendMessage(
+        playerWs,
+        "create_game",
+        JSON.stringify({
+          idGame: gameId,
+          idPlayer: pIndex,
+        }),
+      );
+    }
+  });
+
+  // Remove room from available rooms
+  rooms.delete(data.indexRoom);
+  updateRooms();
+}
